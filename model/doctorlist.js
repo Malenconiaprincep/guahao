4dd7926ec85018c7e65ad3be05122fce66217f62
makuta <wangbo@wepiao.com>
2017-01-20 15:02:08 +0800

    列表界面调整

--- model/doctorlist.js
+++ model/doctorlist.js
@@ -1,67 +1,24 @@
 module.exports = {
   list: [{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
+    name: '杨桂芬',
+    desc: '长期从事妇科临床、教学、科研工作，具有丰富的临床经验。擅长于围产医学以及高危妊娠的产前管理。',
+    protitle: '副主任医师',
+    posttitle: '副教授',
+    pic: 'http://www.znhospital.cn/u/cms/www/201509/061710388x3g.png',
+    hospital: '武汉中南医院'
   },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
+    name: '深怡',
+    desc: '擅长妇科肿瘤的诊断与治疗，在腹腔镜治疗宫颈恶性肿瘤，子宫内膜病变以及卵巢肿瘤方面经验丰富，技术娴熟。在子宫肌瘤，子宫内膜异位症等良性疾病的诊治方面也有丰富的临床经验。',
+    protitle: '副主任医师',
+    posttitle: '副教授',
+    pic: 'http://www.whuh.com/data/attachment/image/20160816/20160816105415_72790.jpg',
+    hospital: '武汉协和医院'
   },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
-  },{
-    name: '皮卡丘',
-    desc: '擅长于围产医院及高危妊娠的产前管理。',
-    pic: 'http://img7.ptpcp.com/v2/thumb/jpg/NjY5MSwwLDAsNCwzLDEsLTEsMSw=/u/6EBC6208E20D6B024A85EE3905E83C11BF6709DB587A64F44141629DE67998ACF20997EDB1E36BC352BBB0FDFAD525D50234048FD3FFDDD8.jpg'
+    name: '闻良珍',
+    desc: '妇产科疑难杂症，尤对围产医学、临床优生、产前诊断（宫内感染、胎儿染色体疾病等）具有独到之处',
+    protitle: '主任医师',
+    posttitle: '教授',
+    pic: 'http://www.tjh.com.cn/plmnhytf12f3/7A57A5A743894A0E/%E4%B8%93%E5%AE%B6%E7%85%A7%E7%89%87/%E5%A6%87%E4%BA%A7%E7%A7%91%E4%BA%A7%E7%A7%91/5%E9%97%BB%E8%89%AF%E7%8F%8D.JPG',
+    hospital: '武汉同济医院'
   }]
 }
